a3701edad44522af7cc81048f52d62a8a754603c
Vishal Shetty <vishal@ampcome.com>
2019-03-16 10:26:37 +0530

    Nested Stated & React Form validations

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import CounterContainer from './Counter/CounterContainer';
+import FormContainer from './Form/FormContainer';
+import NestedComponent from './NestedComponent';
 
-ReactDOM.render(<CounterContainer />, document.getElementById('root'));
+ReactDOM.render(<NestedComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
